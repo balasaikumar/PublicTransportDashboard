19e810e474bed01e30ff37cdb77a7e406afd44d8
SwaroopG <swaroopg@jiyumana-2.local>
2020-08-08 10:35:59 +0530

    Baseline: 1.0.0 - public transport api

---
+++ src/test/java/com/example/publictransportapi/PublicTransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publictransportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
